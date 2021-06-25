923006731141379879c8a2a05ad683837b766813
Luis Cortés <luis.cortes@screeningeagle.com>
2021-06-25 12:33:24 +0200

    new EfficientClassification

--- model.py
+++ model.py
@@ -48,6 +48,83 @@ class EfficientClassification(nn.Module):
         self.effdet = create_model(model_name='efficientdet_d0')
         self.effdet.box_net = nn.Identity()
         self.effdet.class_net = nn.Identity()
+        self.resnet = models.resnet18(pretrained=True)
+
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+        self.deconv0 = nn.ConvTranspose2d(in_channels=64,
+                                          out_channels=16,
+                                          kernel_size=19,
+                                          stride=3,
+                                          padding=1,
+                                          dilation=2)
+
+        self.deconv1 = nn.ConvTranspose2d(in_channels=64,
+                                          out_channels=12,
+                                          kernel_size=9,
+                                          stride=7,
+                                          padding=1,
+                                          dilation=1)
+
+        self.deconv2 = nn.ConvTranspose2d(in_channels=64,
+                                          out_channels=8,
+                                          kernel_size=24,
+                                          stride=9,
+                                          padding=2,
+                                          dilation=4)
+
+        self.deconv3 = nn.ConvTranspose2d(in_channels=64,
+                                          out_channels=4,
+                                          kernel_size=28,
+                                          stride=9,
+                                          padding=1,
+                                          dilation=6)
+
+        self.deconv4 = nn.ConvTranspose2d(in_channels=64,
+                                          out_channels=2,
+                                          kernel_size=30,
+                                          stride=8,
+                                          padding=2,
+                                          dilation=7)
+
+        self.conv0 = nn.Conv2d(in_channels=42,
+                              out_channels=16,
+                              kernel_size=5,
+                              padding=2)
+
+        self.conv1 = nn.Conv2d(in_channels=16,
+                              out_channels=3,
+                              kernel_size=3,
+                              padding=1)
+
+    def forward(self, x):
+        # EffNet + BiFPN
+        fpn_out, _ = self.effdet(x)
+
+        # Convolution Transpose
+        out0 = self.deconv0(fpn_out[0])
+        out1 = self.deconv1(fpn_out[1])
+        out2 = self.deconv2(fpn_out[2])
+        out3 = self.deconv3(fpn_out[3])
+        out4 = self.deconv4(fpn_out[4])
+        deconv_out = torch.cat([out0,out1,out2,out3,out4], dim=1)
+
+        # Convolution
+        conv_out = self.conv1(self.conv0(deconv_out))
+
+        # Resnet18
+        out = self.resnet(conv_out)
+        return out
+
+class EfficientClassification2(nn.Module):
+
+    def __init__(self, num_classes):
+        super(EfficientClassification2, self).__init__()
+        from effdet import create_model
+        self.effdet = create_model(model_name='efficientdet_d0')
+        self.effdet.box_net = nn.Identity()
+        self.effdet.class_net = nn.Identity()
 
         # In features from FPN
         fc_in_features = [64 * i*i for i in [64,32,16,8,4]]
@@ -73,7 +150,7 @@ class EfficientClassification(nn.Module):
 
 if __name__ == '__main__':
 
-    x = torch.randn(1, 3, 512, 512)
+    x = torch.randn(20, 3, 512, 512)
     model = EfficientClassification(num_classes=2)
     fpn_out = model(x)
     print('FIN')
